7c7a7b8c5518de7f72280eb70748fe36875f0539
HIUHIU <88829490+hieungo0503@users.noreply.github.com>
2023-07-05 14:43:40 +0700

    new

---
+++ Blink/common/ouput_iot/output_iot.h
@@ -0,0 +1,13 @@
+#ifndef OUTPUT_IO_H
+#define OUTPUT_IO_H
+
+#include <esp_err.h>
+#include <hal/gpio_types.h>
+
+void output_io_create(gpio_num_t gpio_num);
+
+void output_io_set(gpio_num_t gpio_num, int level);
+
+void output_io_toggle(gpio_num_t gpio_num);
+
+#endif // OUTPUT_IO_H
